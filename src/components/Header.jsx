41bec37539796fe75b336e367315fbf4bd70cf80
spleekz <Shutnik-12K@yandex,ru>
2021-07-01 04:25:02 +0300

    change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,15 @@ import styled from '@emotion/styled'
 import React from 'react'
 
 const HeaderContainer = styled.div`
-  width: 100%;
   background-color: #640000;
   text-align: left;
+  padding: 3px 0px 3px 22px;
 `
 const HeaderTitle = styled.div`
   display: inline-block;
   font-family: 'Homemade Apple', cursive;
   font-size: 36px;
   color: #fdb301;
-  padding: 3px 0px 3px 22px;
   user-select: none;
 `
 
