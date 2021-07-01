af79e6ae92c4927526c5335a7b3891ffad068feb
spleekz <Shutnik-12K@yandex,ru>
2021-07-02 00:24:28 +0300

    add RootStore and Context for it

---
+++ src/stores/RootStore/RootStore.js
@@ -0,0 +1,5 @@
+import { AppStore } from './../AppStore'
+
+export class RootStore {
+  AppStore = new AppStore()
+}
---
+++ src/stores/RootStore/RootStoreContext.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import { createContext } from 'react'
+import { RootStore } from './RootStore'
+
+const RootStoreContext = createContext(null)
+
+export const RootStoreProvider = ({ children }) => {
+  const rootStore = new RootStore()
+  return <RootStoreContext.Provider value={rootStore}>{children}</RootStoreContext.Provider>
+}
+
+export const useStore = () => useContext(RootStoreContext)
