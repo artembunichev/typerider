d8f3b6222d85463c1d2270dbdda5a24401e20e73
spleekz <Shutnik-12K@yandex,ru>
2021-07-02 22:52:08 +0300

    fix url

--- src/API/WordApi.js
+++ src/API/WordApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const WordApi = {
   getRandomWords: (number) => {
-    return axios.get(`https://random-word-api.herokuapp.com//word?number=${number}`).then((response) => {
+    return axios.get(`https://random-word-api.herokuapp.com/word?number=${number}`).then((response) => {
       return response.data
     })
   },
