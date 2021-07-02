b9a901ca86ad1d64eacd1c9ca36712ba5a11440e
spleekz <Shutnik-12K@yandex,ru>
2021-07-03 01:43:50 +0300

    add selecting an active time for race

--- src/pages/Game/GameSettings/GameSettings.jsx
+++ src/pages/Game/GameSettings/GameSettings.jsx
@@ -11,14 +11,37 @@ const Start = styled.button`
   width: 150px;
   height: 45px;
 `
+const TimeForRaceContainer = styled.div``
+const TimeForRace = styled.ul``
+const TimeForRaceItem = styled.li`
+  color: ${(props) => (props.active ? '#33ff00' : '#000000')};
+  &:hover {
+    cursor: pointer;
+  }
+`
 
 export const GameSettings = observer(() => {
   const { GameSettingsStore } = useStore()
   const onStartClick = () => {
     GameSettingsStore.setGameMode(true)
   }
+  const setSelectedTime = (time) => {
+    GameSettingsStore.setSelectedTime(time)
+  }
   return (
     <GameSettingsContainer>
+      <TimeForRaceContainer>
+        How long will your race last?
+        <TimeForRace>
+          {GameSettingsStore.timeForRace.map((t) => {
+            return (
+              <TimeForRaceItem active={t.isActive} key={t.time} onClick={() => setSelectedTime(t.time)}>
+                {t.label}
+              </TimeForRaceItem>
+            )
+          })}
+        </TimeForRace>
+      </TimeForRaceContainer>
       <Start onClick={onStartClick}>Start Game</Start>
     </GameSettingsContainer>
   )
--- src/stores/GameSettingsStore.js
+++ src/stores/GameSettingsStore.js
@@ -5,7 +5,22 @@ export class GameSettingsStore {
     makeAutoObservable(this)
   }
   gameMode = false
+  timeForRace = [
+    { label: '30sec', time: 30000, isActive: true },
+    { label: '60sec', time: 60000, isActive: false },
+    { label: '90sec', time: 90000, isActive: false },
+  ]
+  activeTimeForRace = null
   setGameMode(value) {
     this.gameMode = value
+  }
+  setSelectedTime(time) {
+    this.timeForRace.forEach((t) => {
+      if (t.time === time) {
+        t.isActive = true
+      } else {
+        t.isActive = false
+      }
+    })
   }
 }
