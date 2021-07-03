13a225718d9d0f49bca8d0668985cc5b08af0662
spleekz <Shutnik-12K@yandex,ru>
2021-07-04 01:44:36 +0300

    add : render mistakes count

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
 import { useStore } from '../../stores/RootStore/RootStoreContext'
+import styled from 'styled-components'
+import { Container } from '../Components/Styled/StyledComponents'
+
+const ResultContainer = styled(Container)`
+  background-color: #ff00f2;
+`
+const ErrorsCountContainer = styled.div``
 
 export const Results = observer(() => {
   const { ResultStore } = useStore()
@@ -10,5 +17,9 @@ export const Results = observer(() => {
   if (!ResultStore.isResultReady) {
     history.push('/')
   }
-  return <div className='results'>results</div>
+  return (
+    <ResultContainer>
+      <ErrorsCountContainer>You got {ResultStore.errorsCount} mistakes</ErrorsCountContainer>
+    </ResultContainer>
+  )
 })
