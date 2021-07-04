167025c73e45094363aee8b2f3164410d4557b94
spleekz <Shutnik-12K@yandex,ru>
2021-07-04 22:43:02 +0300

    add vehicles to GameStore

--- src/stores/GameStore.js
+++ src/stores/GameStore.js
@@ -1,5 +1,8 @@
 import { makeAutoObservable, runInAction } from 'mobx'
 import { WordApi } from '../API/WordApi'
+import SportsCar from '../images/Vehicle/sportsCar.svg'
+import Motorbike from '../images/Vehicle/motorbike.svg'
+import Taxi from '../images/Vehicle/taxi.svg'
 
 export class GameStore {
   constructor() {
@@ -15,9 +18,15 @@ export class GameStore {
 
   currentTime = null
 
+  trackLength = 1120
+
   vehiclePosition = 0
   vehicleWidth = 50
-  trackLength = 1120
+  vehicles = [
+    { label: 'sports car', src: SportsCar, isActive: true },
+    { label: 'motorbike', src: Motorbike, isActive: false },
+    { label: 'taxi', src: Taxi, isActive: false },
+  ]
 
   setRandomWords(number) {
     WordApi.getRandomWords(number).then((words) => {
