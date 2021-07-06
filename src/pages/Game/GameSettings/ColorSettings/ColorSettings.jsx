771b7082e2fe61ddf2153bae01e75ed94739dc2c
spleekz <Shutnik-12K@yandex,ru>
2021-07-07 02:11:10 +0300

    add SLetters component

--- src/pages/Game/GameSettings/ColorSettings/ColorSettings.jsx
+++ src/pages/Game/GameSettings/ColorSettings/ColorSettings.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { SCompletedLetters } from './SCompletedLetters/SCompletedLetters'
+import { SLetters } from './SLetters/SLetters'
 
 const ColorSettingsContainer = styled.div``
 
@@ -9,6 +10,7 @@ export const ColorSettings = () => {
     <ColorSettingsContainer>
       Color Settings
       <SCompletedLetters />
+      <SLetters />
     </ColorSettingsContainer>
   )
 }
---
+++ src/pages/Game/GameSettings/ColorSettings/SLetters/SLetters.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { observer } from 'mobx-react-lite'
+import styled from 'styled-components'
+import { useStore } from '../../../../../stores/RootStore/RootStoreContext'
+
+const SLettersContainer = styled.div``
+const SLettersInput = styled.input``
+
+export const SLetters = observer(() => {
+  const { GameSettingsStore } = useStore()
+
+  const setLettersColor = (e) => {
+    GameSettingsStore.setLettersColor(e.target.event)
+  }
+  return (
+    <SLettersContainer>
+      <SLettersInput type='color' onChange={setLettersColor} value={GameSettingsStore.lettersColor} />
+    </SLettersContainer>
+  )
+})
