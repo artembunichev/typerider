c0c45507368636039d75a930536f5e30cdafd05b
spleekz <Shutnik-12K@yandex,ru>
2021-07-07 16:59:11 +0300

    add wordsSettings folder

--- src/pages/Game/GameSettings/WordsSettings/ColorSettings/SLetters/SLetters.jsx
+++ src/pages/Game/GameSettings/WordsSettings/ColorSettings/SLetters/SLetters.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
-import { useStore } from '../../../../../stores/RootStore/RootStoreContext'
+import { useStore } from '../../../../../../stores/RootStore/RootStoreContext'
 
 const SLettersContainer = styled.div``
 const SLettersInput = styled.input``
