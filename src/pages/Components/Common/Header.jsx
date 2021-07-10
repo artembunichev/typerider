545eb7d043d0152defbeabe391e611f9823dbd86
spleekz <Shutnik-12K@yandex,ru>
2021-07-10 23:05:10 +0300

    clear nickname input value after going to game

--- src/pages/Components/Common/Header.jsx
+++ src/pages/Components/Common/Header.jsx
@@ -42,6 +42,7 @@ export const Header = observer(() => {
 
   const goToHistoryPage = () => {
     history.push('/history')
+    AppStore.setUserNickname('')
     setOnHistoryPage(true)
   }
   const goToHomePage = () => {
