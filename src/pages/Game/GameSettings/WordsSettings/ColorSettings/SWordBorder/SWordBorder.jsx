cd32d595dc0988fbb27ee837cc20c82f170e7a8f
spleekz <Shutnik-12K@yandex,ru>
2021-07-11 02:35:48 +0300

    prettier refactor in SWordBorder

--- src/pages/Game/GameSettings/WordsSettings/ColorSettings/SWordBorder/SWordBorder.jsx
+++ src/pages/Game/GameSettings/WordsSettings/ColorSettings/SWordBorder/SWordBorder.jsx
@@ -1,15 +1,15 @@
 import React, { useContext } from 'react'
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
-import { GameStoreContext, useStore } from '../../../../../../stores/RootStore/RootStoreContext';
+import { GameStoreContext, useStore } from '../../../../../../stores/RootStore/RootStoreContext'
 
 const WordBorderContainer = styled.div``
 const WordBorderInput = styled.input``
 
 export const SWordBorder = observer(() => {
-  const {AppStore} = useStore()
-  const {GameSettingsState} = useContext(GameStoreContext)
-  
+  const { AppStore } = useStore()
+  const { GameSettingsState } = useContext(GameStoreContext)
+
   const setWordBorder = (e) => {
     GameSettingsState.setWordBorder(e.target.value)
   }
