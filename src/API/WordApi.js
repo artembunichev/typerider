cdfdb67b474f367408af5a7ed78212899be6daa0
spleekz <Shutnik-12K@yandex,ru>
2021-07-10 23:34:47 +0300

    delete word Api

--- src/API/WordApi.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const WordApi = {
-  getRandomWords: (number) => {
-    return axios.get(`https://random-word-api.herokuapp.com/word?number=${number}`).then((response) => {
-      return response.data
-    })
-  },
-}
