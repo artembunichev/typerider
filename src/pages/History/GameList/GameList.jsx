1fe76082794ecc715c6e09e6bb2f0feed6d651d6
spleekz <Shutnik-12K@yandex,ru>
2021-07-16 23:57:56 +0300

    add scroll for history game list

--- src/pages/History/GameList/GameList.jsx
+++ src/pages/History/GameList/GameList.jsx
@@ -1,14 +1,42 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import { GameBlock } from './GameBlock/GameBlock'
 
-const GameListContainer = styled.div``
+const GameListContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+const GameListWrapper = styled.div`
+  width: 860px;
+  display: flex;
+  justify-content: center;
+`
+const StyledGameList = styled.div`
+  width: 68%;
+  max-height: 670px;
+  overflow-y: ${(props) => (props.listHeight > 670 ? 'scroll' : 'none')};
+  padding: 5px;
+`
 
 export const GameList = (props) => {
+  const [listHeight, setListHeight] = useState(0)
+  const gameListRef = useRef(null)
+  useEffect(() => {
+    setListHeight(gameListRef.current.offsetHeight)
+  }, [gameListRef.current])
+
   const GameListItems = props.sortedHistory.map((game) => {
     return <GameBlock key={game.date} game={game} />
   })
 
-  return <GameListContainer>{GameListItems}</GameListContainer>
+  return (
+    <GameListContainer>
+      <GameListWrapper>
+        <StyledGameList ref={gameListRef} listHeight={listHeight}>
+          {GameListItems}
+        </StyledGameList>
+      </GameListWrapper>
+    </GameListContainer>
+  )
 }
