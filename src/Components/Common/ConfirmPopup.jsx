61bda3c1912d3dd4d605fe5c6dc759174eed0720
spleekz <Shutnik-12K@yandex,ru>
2021-07-19 19:03:59 +0300

    total rework popup styles

--- src/Components/Common/ConfirmPopup.jsx
+++ src/Components/Common/ConfirmPopup.jsx
@@ -1,73 +1,106 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import styled from 'styled-components'
+import { Bold } from '../Styled/StyledComponents'
 
 const PopupContainer = styled.div`
+  position: fixed;
+  z-index: 9999;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.534);
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100vw;
-  height: 100vh;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  z-index: 999;
   opacity: ${(props) => (props.isPopup ? 1 : 0)};
-  pointer-events: ${(props) => (props.isPopup ? 'auto' : 'none')};
+  pointer-events: ${(props) => (props.isPopup ? 'all' : 'none')};
   transition: opacity 0.2s;
-  background-color: rgba(0, 0, 0, 0.52);
 `
-const PopupForm = styled.div`
-  width: 50%;
-  height: 50%;
-  background-color: #f8f8f8;
-  border-radius: 20px;
+const PopupBlock = styled.div`
+  margin: 32px;
+  overflow-x: auto;
+  display: flex;
+  align-items: center;
+  position: relative;
+  width: 920px;
+  height: 380px;
+  max-width: 90%;
+  max-height: 90%;
+  background-color: #ffffff;
+  border-radius: 6px;
   transform: ${(props) => (props.isPopup ? 'scale(1)' : 'scale(0)')};
   transition: transform 0.4s;
 `
-const PopupFormContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  padding: 25px;
+const PopupBlockContent = styled.div`
+  width: 100%;
+  padding: 32px;
 `
-const PopupFormTitle = styled.div`
-  font-size: 39px;
+const PopupTitle = styled.div`
+  @media (max-width: 768px) {
+    font-size: 2.5vw;
+  }
+  @media (min-width: 2000px) {
+    font-size: 1vw;
+  }
+  font-size: 32px;
+  text-align: center;
+  margin-bottom: 25px;
 `
 const PopupButtonsContainer = styled.div`
   display: flex;
+  justify-content: center;
+`
+const PopupButtons = styled.div`
+  display: flex;
+  width: 60%;
   justify-content: space-between;
-  width: 500px;
 `
 const PopupButton = styled.button`
+  @media (max-width: 768px) {
+    font-size: 1.9vw;
+    width: 20vw;
+    height: 10vh;
+  }
+  @media (min-width: 2000px) {
+    font-size: 0.7vw;
+    width: 5vw;
+    height: 4vh;
+  }
   width: 200px;
-  height: 74px;
-  font-size: 30px;
+  height: 70px;
+  font-size: 29px;
+  margin: 0 6px;
   &:hover {
     cursor: pointer;
   }
 `
 const PopupYesButton = styled(PopupButton)`
-  background-color: #89ff5a;
+  background-color: #7aff5f;
 `
 const PopupNoButton = styled(PopupButton)`
-  background-color: #ff5a5a;
+  background-color: #ff5f5f;
 `
 
 export const ConfirmPopup = ({ isPopup, title, yesFunction, noFunction }) => {
   return (
     <PopupContainer isPopup={isPopup}>
-      <PopupForm isPopup={isPopup}>
-        <PopupFormContent>
-          <PopupFormTitle>{title}</PopupFormTitle>
+      <PopupBlock isPopup={isPopup}>
+        <PopupBlockContent>
+          <PopupTitle>
+            <Bold>{title}</Bold>
+          </PopupTitle>
           <PopupButtonsContainer>
-            <PopupYesButton onClick={yesFunction}>YES</PopupYesButton>
-            <PopupNoButton onClick={noFunction}>NO</PopupNoButton>
+            <PopupButtons>
+              <PopupYesButton onClick={yesFunction}>YES</PopupYesButton>
+              <PopupNoButton onClick={noFunction}>NO</PopupNoButton>
+            </PopupButtons>
           </PopupButtonsContainer>
-        </PopupFormContent>
-      </PopupForm>
+        </PopupBlockContent>
+      </PopupBlock>
     </PopupContainer>
   )
 }
