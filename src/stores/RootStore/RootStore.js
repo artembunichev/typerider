d567c1b1cd48f01628107f1fc80cd8de7afc3fbf
spleekz <Shutnik-12K@yandex,ru>
2021-07-25 04:36:42 +0300

    install mobx-remotedev

--- src/stores/RootStore/RootStore.js
+++ src/stores/RootStore/RootStore.js
@@ -3,15 +3,16 @@ import { HistoryStore } from '../HistoryStore'
 import { GameStore } from '../GameStore'
 import { GameSettingsStore } from '../GameSettingsStore'
 import autoBind from 'auto-bind'
+import remotedev from 'mobx-remotedev'
 
 export class RootStore {
   constructor() {
     autoBind(this)
   }
-  AppStore = new AppStore()
-  HistoryStore = new HistoryStore()
-  GameSettingsStore = new GameSettingsStore()
+  AppStore = remotedev(new AppStore(), { name: 'AppStore' })
+  HistoryStore = remotedev(new HistoryStore(), { name: 'HistoryStore' })
+  GameSettingsStore = remotedev(new GameSettingsStore(), { name: 'GameSettingsStore' })
   createGameStore() {
-    return new GameStore(this.GameSettingsStore)
+    return remotedev(new GameStore(this.GameSettingsStore), { name: 'GameStore' })
   }
 }
