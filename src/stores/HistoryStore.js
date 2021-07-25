e755608856b8affe482622137969176f82d714cd
spleekz <Shutnik-12K@yandex,ru>
2021-07-25 20:25:33 +0300

    save game history to local storage

--- src/stores/HistoryStore.js
+++ src/stores/HistoryStore.js
@@ -1,50 +1,13 @@
 import { makeAutoObservable } from 'mobx'
-// import { makePersistable } from 'mobx-persist-store'
+import { makePersistable } from 'mobx-persist-store'
 
 export class HistoryStore {
   constructor() {
     makeAutoObservable(this)
-    // makePersistable(this, { name: 'HistoryStore', properties: ['gameHistory'], storage: window.localStorage })
+    makePersistable(this, { name: 'HistoryStore', properties: ['gameHistory'], storage: window.localStorage })
   }
 
-  gameHistory = [
-    {
-      correctWordsCount: 1,
-      date: 1626197504109,
-      errorsCount: 12,
-      raceTime: 30,
-      typeSpeed: 10,
-      userNickname: '123',
-      vehicle: '/static/media/sportsCar.d9e9193c.svg',
-    },
-    {
-      correctWordsCount: 14,
-      date: 16261344709,
-      errorsCount: 3,
-      raceTime: 60,
-      typeSpeed: 10,
-      userNickname: 'spleekz',
-      vehicle: '/static/media/motorbike.2a6f0296.svg',
-    },
-    {
-      correctWordsCount: 14,
-      date: 1626123409,
-      errorsCount: 3,
-      raceTime: 60,
-      typeSpeed: 10,
-      userNickname: 'spleekz',
-      vehicle: '/static/media/motorbike.2a6f0296.svg',
-    },
-    {
-      correctWordsCount: 14,
-      date: 1626197504709,
-      errorsCount: 3,
-      raceTime: 60,
-      typeSpeed: 10,
-      userNickname: 'spleekz',
-      vehicle: '/static/media/motorbike.2a6f0296.svg',
-    },
-  ]
+  gameHistory = []
 
   filters = [
     { name: 'By type speed', filter: 'typeSpeed' },
@@ -66,7 +29,7 @@ export class HistoryStore {
   deleteCurrentGame(gameDate) {
     this.gameHistory = this.gameHistory.filter((game) => game.date !== gameDate)
   }
-  
+
   get activeFilterValue() {
     return this.activeFilter.filter
   }
