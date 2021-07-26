d90026a37a7243df1ca031e4d826b5405e0ee50a
spleekz <Shutnik-12K@yandex,ru>
2021-07-26 20:50:36 +0300

    add adaptive for home page

--- src/pages/Main/Welcome.jsx
+++ src/pages/Main/Welcome.jsx
@@ -13,24 +13,60 @@ const WelcomeContainer = styled(Container)`
   padding: 30px 0;
   background-color: #93c2ff;
 `
-const WelcomeSubTitle = styled.div`
+const WelcomeTitle = styled.div`
+  @media (max-width: 650px) {
+    font-size: 54px;
+  }
+  @media (max-width: 540px) {
+    font-size: 45px;
+  }
+  @media (max-width: 370px) {
+    font-size: 39px;
+  }
   font-size: 70px;
+  margin: 25px 25px 25px 8px;
+  text-align: center;
 `
 const WelcomeInput = styled.input`
-  margin-top: 16px;
+  margin-top: 8px;
   font-size: 24px;
   border-radius: 9px;
   padding: 9px;
+  @media (max-width: 650px) {
+    width: 460px;
+  }
+  @media (max-width: 540px) {
+    width: 395px;
+  }
+  @media (max-width: 470px) {
+    width: 350px;
+    height: 37px;
+  }
+  @media (max-width: 370px) {
+    width: 300px;
+    height: 33px;
+  }
   width: 500px;
   height: 46px;
   border: none;
   outline: none;
 `
-const LetsGo = styled.button`
-  font-size: 22px;
+const LetsGoButton = styled.button`
   margin-top: 16px;
+  @media (max-width: 540px) {
+    width: 300px;
+  }
+  @media (max-width: 470px) {
+    width: 240px;
+    height: 30px;
+  }
+  @media (max-width: 370px) {
+    width: 200px;
+    font-size: 17px;
+  }
   width: 340px;
   height: 38px;
+  font-size: 22px;
   border-radius: 9px;
   &:hover {
     cursor: pointer;
@@ -60,7 +96,7 @@ export const Welcome = observer(() => {
 
   return (
     <WelcomeContainer>
-      <WelcomeSubTitle>Your nickname for the next race?</WelcomeSubTitle>
+      <WelcomeTitle>Your nickname for the next race?</WelcomeTitle>
       <WelcomeInput
         autoFocus={true}
         maxLength='16'
@@ -68,7 +104,7 @@ export const Welcome = observer(() => {
         onKeyPress={onEnterPress}
         value={inputValue}
       />
-      <LetsGo onClick={goToGame}>Let&apos;s go!</LetsGo>
+      <LetsGoButton onClick={goToGame}>Let&apos;s go!</LetsGoButton>
     </WelcomeContainer>
   )
 })
