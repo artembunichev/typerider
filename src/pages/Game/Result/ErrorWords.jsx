db826eb078d205ccc83eef63731971ae8924f664
spleekz <Shutnik-12K@yandex,ru>
2021-07-28 12:22:25 +0300

    add animation for showing error words

--- src/pages/Game/Result/ErrorWords.jsx
+++ src/pages/Game/Result/ErrorWords.jsx
@@ -2,19 +2,37 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const ErrorWordsWrapper = styled.div``
+const ErrorWordsWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`
+
 const ErrorWordsContainer = styled.ul`
-  display: ${(props) => (props.isWordsVisible ? 'flex' : 'none')};
-  justify-content: space-around;
+  display: flex;
+  position: ${(props) => (props.isPositionAbsolute ? 'absolute' : 'relative')};
+  right: ${(props) => (props.isWordsVisible ? '0px' : '280px')};
+  opacity: ${(props) => (props.isWordsVisible ? '1' : '0')};
+  top: 0px;
+  justify-content: start;
   max-width: 330px;
   border-radius: 5px;
   padding: 6px;
   flex-wrap: wrap;
   background-color: #314a68;
   list-style: none;
+  transition: 0.38s;
 `
 const ErrorWordsPicker = styled.div``
-const ErrorWordsPickerTitle = styled.div``
+const ErrorWordsPickerTitle = styled.div`
+  user-select: none;
+  &:hover {
+    cursor: pointer;
+  }
+`
 const ErrorWord = styled.li`
   font-size: 32px;
   background-color: #dfdfdf;
@@ -28,12 +46,21 @@ const ErrorLetter = styled.span`
 
 export const ErrorWords = (props) => {
   const [isWordsVisible, setIsWordsVisible] = useState(false)
+  const [isPositionAbsolute, setIsPositionAbsolute] = useState(true)
 
-  const showWords = () => {
-    setIsWordsVisible(true)
+  const showWords = (e) => {
+    if (!isPositionAbsolute) {
+      e.preventDefault()
+    } else {
+      setIsWordsVisible(true)
+      setIsPositionAbsolute(false)
+    }
   }
   const hideWords = () => {
     setIsWordsVisible(false)
+    setTimeout(() => {
+      setIsPositionAbsolute(true)
+    }, 380)
   }
 
   return (
@@ -45,7 +72,7 @@ export const ErrorWords = (props) => {
           <ErrorWordsPickerTitle onClick={showWords}>Show Error Words</ErrorWordsPickerTitle>
         )}
       </ErrorWordsPicker>
-      <ErrorWordsContainer isWordsVisible={isWordsVisible}>
+      <ErrorWordsContainer isWordsVisible={isWordsVisible} isPositionAbsolute={isPositionAbsolute}>
         {props.words.map((word) => {
           const key = word.letters.reduce((key, letter) => {
             key = key + letter.id
