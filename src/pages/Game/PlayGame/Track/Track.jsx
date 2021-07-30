7433efc071e6d961bfb3da3eeebb77a829a41ac0
spleekz <Shutnik-12K@yandex,ru>
2021-07-30 22:24:28 +0300

    add finish flag to the end of the track

--- src/pages/Game/PlayGame/Track/Track.jsx
+++ src/pages/Game/PlayGame/Track/Track.jsx
@@ -4,16 +4,28 @@ import styled from 'styled-components'
 import { Vehicle } from './Vehicle'
 import { GameStoreContext, useStore } from '../../../../stores/RootStore/RootStoreContext'
 import { useScreenWidth } from '../../../../assets/hooks/useScreenWidth'
+import Flag from '../../../../assets/images/flag.svg'
 
 const TrackContainer = styled.div`
   display: flex;
   justify-content: center;
-  padding: 50px;
+  padding-bottom: 50px;
 `
 const StyledTrack = styled.div`
   position: relative;
   width: 100%;
   border-bottom: 3px dashed #000000;
+`
+const FlagContainer = styled.div`
+  position: absolute;
+  right: -45px;
+  bottom: 5px;
+  width: 64px;
+  height: 64px;
+  background-image: ${(props) => `url(${props.bgImage})`};
+  background-repeat: no-repeat;
+  background-size: cover;
+  transform: rotate(-13deg);
 `
 
 export const Track = observer(() => {
@@ -33,6 +45,7 @@ export const Track = observer(() => {
     <TrackContainer>
       <StyledTrack ref={trackRef} length={PlayGameState.trackLength}>
         <Vehicle model={GameSettingsStore.activeVehicleModel} />
+        <FlagContainer bgImage={Flag} />
       </StyledTrack>
     </TrackContainer>
   )
