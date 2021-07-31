b11429b89275c7ef69310ba733e42ed4ced102a9
spleekz <Shutnik-12K@yandex,ru>
2021-07-31 20:17:07 +0300

    add styled title component to history page

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { Bold, Container } from '../../Components/Styled/StyledComponents'
+import { Bold, Container, Title } from '../../Components/Styled/StyledComponents'
 import { GameList } from './GameList/GameList'
 import { HistoryFilters } from './HistoryFilters/HistoryFilters'
 import { observer } from 'mobx-react-lite'
@@ -19,32 +19,6 @@ const HistoryWrapper = styled(Container)`
   flex-direction: column;
   justify-content: center;
   background-color: #e0e3e8;
-`
-const HistoryTitle = styled.div`
-  @media (max-width: 650px) {
-    font-size: 44px;
-  }
-  @media (max-width: 540px) {
-    font-size: 40px;
-  }
-  @media (max-width: 490px) {
-    font-size: 35px;
-  }
-  @media (max-width: 440px) {
-    font-size: 31px;
-  }
-  @media (max-width: 390px) {
-    font-size: 28px;
-  }
-  @media (max-width: 360px) {
-    font-size: 26px;
-  }
-  font-size: 48px;
-  text-align: center;
-  color: #ff820d;
-  padding: 0 8px 8px 8px;
-  background-color: #000000;
-  border-radius: 0px 0px 7px 7px;
 `
 const NoGameHistory = styled.div`
   padding: 5px 20px;
@@ -130,9 +104,9 @@ export const History = observer(() => {
 
   return (
     <HistoryContainer>
-      <HistoryTitle>
+      <Title>
         <Bold>Your Game History</Bold>
-      </HistoryTitle>
+      </Title>
       {HistoryStore.gameHistory.length ? (
         <HistoryWrapper>
           <HistoryFilters setIsPopup={setIsClearHistoryPopup} />
