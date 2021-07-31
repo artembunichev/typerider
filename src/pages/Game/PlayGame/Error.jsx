0dd373f49ce39e3a6e8d871fc707d14830473f3f
spleekz <Shutnik-12K@yandex,ru>
2021-07-31 20:15:53 +0300

    add animation for error

--- src/pages/Game/PlayGame/Error.jsx
+++ src/pages/Game/PlayGame/Error.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { Bold } from '../../../Components/Styled/StyledComponents'
 
-const ErrorContainer = styled.div`
-  background-color: #ff0000;
+const ErrorWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
 `
-const ErrorWrapper = styled.span`
-  font-size: 24px;
+const ErrorContainer = styled.div`
+  background-color: ${(props) => (props.isLighting ? '#ff0000' : '#ffaaaa')};
+  border-radius: 9px;
+  padding: 19px;
+  transition: background-color 0.2s ease-in;
+`
+const ErrorText = styled.span`
+  color: #000000;
+  font-size: 50px;
+  user-select: none;
 `
 
 export const Error = () => {
+  const [isLighting, setIsLighting] = useState(true)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsLighting((prev) => !prev)
+    }, 200)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
-    <ErrorContainer>
-      <ErrorWrapper>Fix a mistake!</ErrorWrapper>
-    </ErrorContainer>
+    <ErrorWrapper>
+      <ErrorContainer isLighting={isLighting}>
+        <ErrorText>
+          <Bold>Fix a mistake!</Bold>
+        </ErrorText>
+      </ErrorContainer>
+    </ErrorWrapper>
   )
 }
